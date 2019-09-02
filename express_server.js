398913c62fb3a9746c3a25e1bf0c0a072a4fdded
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-02 22:15:59 +0000

    added ejs file for urls route

--- express_server.js
+++ express_server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get('/', (req, res) => {
-  res.send('Hello!');
+app.get('/urls', (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
