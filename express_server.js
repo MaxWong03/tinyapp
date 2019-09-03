0fc4084cc7259e00edcec3e22d770bad7aa81cf3
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-03 06:09:09 +0000

    added delete url feature

--- express_server.js
+++ express_server.js
@@ -14,10 +14,15 @@ const generateRandomString = () => {
   return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5).toUpperCase();
 };
 
+
+
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 app.get('/urls', (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
@@ -46,19 +51,18 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-app.get('/u/:shortURL', (req, res) => { //redirecting
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls'); //redirect the client back to the url_index page
+});
+
+app.get('/u/:shortURL', (req, res) => { //redirecting to longURL
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
 
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
