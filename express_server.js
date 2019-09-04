3653d0cfb59f24f6dd3c4de9303ba7712532505c
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-04 03:07:34 +0000

    reorganize codes for routes`

--- express_server.js
+++ express_server.js
@@ -1,26 +1,19 @@
 const {bodyParser, morgan, app, PORT, cookieParser, generateRandomString, getUserByEmail, urlDatabase, users}
 = require('./constants');
 
+
+//server engine set up
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(morgan('dev'));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.clearCookie('user_id');
-  res.redirect('/urls');
-});
 
+//GET Requests Routes
 app.get('/urls', (req, res) => {
   const userID = req.cookies['user_id'];
   let templateVars = { urls: urlDatabase, user: users[userID] };
   res.render('urls_index', templateVars);
-});
-
-app.get('/register', (req, res) => {
-  const userID = req.cookies['user_id'];
-  let templateVars = { urls: urlDatabase, user: users[userID] };
-  res.render('register', templateVars);
 });
 
 app.get('/login', (req, res) => {
@@ -29,10 +22,39 @@ app.get('/login', (req, res) => {
   res.render('login', templateVars);
 });
 
+app.get('/register', (req, res) => {
+  const userID = req.cookies['user_id'];
+  let templateVars = { urls: urlDatabase, user: users[userID] };
+  res.render('register', templateVars);
+});
+
 app.get('/urls/new', (req, res) => {
   const userID = req.cookies['user_id'];
   let templateVars = { urls: urlDatabase, user: users[userID] };
   res.render('urls_new', templateVars);
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  const userID = req.cookies['user_id'];
+  let templateVars = { shortURL, longURL, user: users[userID] };
+  res.render('urls_show', templateVars);
+});
+
+app.get('/u/:shortURL', (req, res) => { //redirecting to longURL
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
+
+//POST requests routes
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.post('/login', (req, res) => {
@@ -67,34 +89,9 @@ app.post('/register', (req, res) => {
   }
 });
 
-app.post('/urls', (req, res) => {
-  const shortURL = generateRandomString();
-  const longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
-
-app.get('/urls/:shortURL', (req, res) => {
-  const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
-  const userID = req.cookies['user_id'];
-  let templateVars = { shortURL, longURL, user: users[userID] };
-  res.render('urls_show', templateVars);
-});
-
-app.get('/u/:shortURL', (req, res) => { //redirecting to longURL
-  const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
-  res.redirect(longURL);
-});
-
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls'); //redirect the client back to the url_index page
-});
-
-app.post('/urls/:shortURL', (req, res) => { //when user click edit in index page
-  res.redirect(`/urls/${req.params.shortURL}`);
 });
 
 app.post('/urls/:shortURL/edit', (req, res) => {//routing the edit action (when user edit longurl of a correspond shorturl via submit)
@@ -102,6 +99,10 @@ app.post('/urls/:shortURL/edit', (req, res) => {//routing the edit action (when 
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = newURL;
   res.redirect('/urls');
+});
+
+app.post('/urls/:shortURL', (req, res) => { //when user click edit in index page
+  res.redirect(`/urls/${req.params.shortURL}`);
 });
 
 app.listen(PORT, () => {
